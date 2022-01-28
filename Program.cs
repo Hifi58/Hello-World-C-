b5cf1b7137c62758a30a61e941c35bf4b19610f3
Hifi58 <anaisbiquena@macbook-pro-anais.home>
2022-01-28 14:40:34 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int my_int = 5;
+            int your_int= 10;
+
+            int total = my_int + your_int;
+
+            Console.WriteLine("==============");
+            Console.WriteLine("==============");
+            Console.WriteLine(total);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("==============");
+        }
+    }
